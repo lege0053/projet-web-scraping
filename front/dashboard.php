35caaa3f9431747e4d58115ecf0e9b454fc6cca7
Lucie Fossier <lucie.fossier@aviation-civile.gouv.fr>
2024-03-25 16:03:06 +0100

    feat : affichage de toutes les actions style ✨

--- front/dashboard.php
+++ front/dashboard.php
@@ -3,21 +3,17 @@
 declare(strict_types=1);
 
 require_once "autoload.php";
-
 require_once "../api/action.php";
 
 $webpage = new WebPage("Dashboard");
 
 $webpage->appendContent(<<<HTML
     <div class="">
-        <h1>Dashboard</h1>
-        <p></p>
-
-    </div>
-  HTML);
+        <h1 class="titre">Dashboard</h1>
+            <div class="cards">
+HTML);
 
 $actions = Action::getAll();
-
 
 // Supposons que $actions soit le tableau contenant vos objets "Action"
 foreach ($actions as $action) {
@@ -25,36 +21,38 @@ foreach ($actions as $action) {
     $reflectionClass = new ReflectionClass($action);
     $properties = $reflectionClass->getProperties(ReflectionProperty::IS_PRIVATE);
 
-    // Boucle à travers les propriétés
+    // Début de la boucle pour chaque action
     foreach ($properties as $property) {
         // Rendre la propriété accessible
         $property->setAccessible(true);
-        
+
         // Afficher le nom de la propriété
         $propertyName = $property->getName();
-        
-       
 
         // Afficher la valeur de la propriété
         $propertyValue = $property->getValue($action);
 
+        $webpage->appendContent(<<<HTML
+            <div class="action">
+                <h1 > $propertyValue[Id_Action] </h1>
+        HTML);
+
         foreach ($propertyValue  as $key => $value) {
-            //var_dump($key,$value);
             $webpage->appendContent(<<<HTML
-                <p>$key : $value <p>
+                <p>$key : $value </p>
             HTML);
         }
-        
 
+        $webpage->appendContent(<<<HTML
+            </div> <!-- Fermeture de la balise div "action" -->
+        HTML);
     }
+    // Fin de la boucle pour chaque action
 }
 
-
-
-
-
-
-
-  
+$webpage->appendContent(<<<HTML
+    </div> <!-- Fermeture de la balise div "cards" -->
+</div> <!-- Fermeture de la balise div principale -->
+HTML);
 
 echo $webpage->toHTML();
