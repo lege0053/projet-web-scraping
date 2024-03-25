4d2cfa487bf6739856a8321a79eabb996d80f2c8
Marine Legendre <marine.leg02@gmail.com>
2024-03-25 19:21:28 +0100

    wip: scrap le dernier message du forum 🚧

--- api/scrapingForum.php
+++ api/scrapingForum.php
@@ -26,9 +26,23 @@ $session->visit($url);
 $page = $session->getPage();
 
 // récupération des données
+$subject = $page->find('css','table > tbody > tr:nth-child(1) > td:nth-child(2) > div > a');
+$subject->click();
 
-// mise en db
+$auteur = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div:nth-child(3) > div > div.c-block > div.c-block__body.c-block__body--keep-border\@xs-max.c-block__body--keep-padding\@xs-max > div.c-message > div.c-profile-card.\/.o-flag.o-flag--top > div.c-profile-card__body.\/.o-flag__body > div > div.o-ellipsis-container > div.c-profile-card__name > button')->getText();
+var_dump($auteur);
 
-system('pause');
+$date = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div:nth-child(3) > div > div.c-block > div.c-block__body.c-block__body--keep-border\@xs-max.c-block__body--keep-padding\@xs-max > div.c-message > div.c-profile-card.\/.o-flag.o-flag--top > div.c-profile-card__body.\/.o-flag__body > div > div.c-source > span:nth-child(1)')->getText();
+var_dump($date);
+
+$heure = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div:nth-child(3) > div > div.c-block > div.c-block__body.c-block__body--keep-border\@xs-max.c-block__body--keep-padding\@xs-max > div.c-message > div.c-profile-card.\/.o-flag.o-flag--top > div.c-profile-card__body.\/.o-flag__body > div > div.c-source > span:nth-child(3)')->getText();
+var_dump($heure);
+
+$contenu = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div:nth-child(3) > div > div.c-block > div.c-block__body.c-block__body--keep-border\@xs-max.c-block__body--keep-padding\@xs-max > div.c-message > p')->getText();
+var_dump($contenu);
+
+$retour = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > a:nth-child(2)');
+$retour->click();
+
 
 
