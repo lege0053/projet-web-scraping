72335bf072fbcdbd392ae8fd9d7b878405a5b9b4
Marine Legendre <marine.leg02@gmail.com>
2024-03-29 19:17:42 +0100

    fix: téléchargement du excel 🩹

--- api/scrapingHistorique.php
+++ api/scrapingHistorique.php
@@ -9,7 +9,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/');
 $dotenv->load();
 
 $mink = new Mink(array(
-  'browser' => new Session(new ChromeDriver('http://localhost:9222', null, 'http://www.google.com')),
+  'browser' => new Session(new ChromeDriver('http://localhost:9222', null, 'http://www.google.com', ['downloadBehavior'=> 'allow','downloadPath'=> __DIR__])),
 ));
 
 $mink->setDefaultSessionName('browser');
@@ -33,5 +33,34 @@ $inputPassword->setValue($_ENV['MDP']);
 // bouton connexion
 $btnConnexion = $page->find('css','#login_member_connect');
 $btnConnexion->click();
+
+sleep(2);
+
+$session->visit('https://www.boursorama.com/connexion/?org=/espace-membres/telecharger-cours/paris');
+$page = $session->getPage();
+
+$btnFormat = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div > div > form > div.o-grid.o-grid--middle > div:nth-child(1) > div > div > div.c-select__textbox');
+$btnFormat->click();
+
+$excelFormat = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div > div > form > div.o-grid.o-grid--middle > div:nth-child(1) > div > div > div.c-select__listbox.is-open > div:nth-child(9)');
+$excelFormat->click();
+
+$btnDownload = $page->find('css','input[value="Télécharger"]');
+$btnDownload->click();
+
+$date = $page->find('css', '#quote_search_endDate')->getValue();
+$date = str_replace('/', '-', $date);
+$date = strtotime($date);
+$date = date('Y-m-d', $date);
+$date = str_replace('-', '_', $date);
+var_dump($date);
+
+$f="SRD_".$date.".xls";
+$essai = 1;
+while (!file_exists($f) && $essai<10) {
+  sleep(1);
+  $essai++;
+}
+
 
 system('pause');
