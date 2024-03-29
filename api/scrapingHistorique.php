90c32d18ffc70fcd95ac3b12e00264e8c5114a7f
Marine Legendre <marine.leg02@gmail.com>
2024-03-29 20:17:56 +0100

    feat: téléchargement par type d'actions ✨

--- api/scrapingHistorique.php
+++ api/scrapingHistorique.php
@@ -12,6 +12,8 @@ $mink = new Mink(array(
   'browser' => new Session(new ChromeDriver('http://localhost:9222', null, 'http://www.google.com', ['downloadBehavior'=> 'allow','downloadPath'=> __DIR__])),
 ));
 
+$actions = $_POST['actions'];
+
 $mink->setDefaultSessionName('browser');
 $session = $mink->getSession();
 
@@ -20,47 +22,57 @@ $page = $session->getPage();
 
 // espace membre
 $btnMembre = $page->find('css','#login-member');
-$btnMembre->click();
+// si déjà connecté
+if($btnMembre) {
+  $btnMembre->click();
 
-// login
-$inputPseudo = $page->find('css','#login_member_login');
-$inputPseudo->setValue($_ENV['PSEUDO']);
+  // login
+  $inputPseudo = $page->find('css','#login_member_login');
+  $inputPseudo->setValue($_ENV['PSEUDO']);
 
-// password
-$inputPassword = $page->find('css','#login_member_password');
-$inputPassword->setValue($_ENV['MDP']);
+  // password
+  $inputPassword = $page->find('css','#login_member_password');
+  $inputPassword->setValue($_ENV['MDP']);
 
-// bouton connexion
-$btnConnexion = $page->find('css','#login_member_connect');
-$btnConnexion->click();
+  // bouton connexion
+  $btnConnexion = $page->find('css','#login_member_connect');
+  $btnConnexion->click();
 
-sleep(2);
+  sleep(2);
+}
+
 
 $session->visit('https://www.boursorama.com/connexion/?org=/espace-membres/telecharger-cours/paris');
 $page = $session->getPage();
 
-$btnFormat = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div > div > form > div.o-grid.o-grid--middle > div:nth-child(1) > div > div > div.c-select__textbox');
-$btnFormat->click();
+// actions
+$btnActions = $page->find('css', '#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div > div > form > div:nth-child(3) > div > ul > li.o-list__item.u-1\/2\@sm-min.u-1\/3\@md-min > div.c-select.o-accordion-content > div > div.c-select__textbox');
+$btnActions->click(); //ouvre le select
+$selectActions = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div > div > form > div:nth-child(3) > div > ul > li.o-list__item.u-1\/2\@sm-min.u-1\/3\@md-min > div.c-select.o-accordion-content.is-expanded > div > div.c-select__listbox.is-open > div:nth-child('.$actions.')');
+$selectActions->click();
 
+// format
+$btnFormat = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div > div > form > div.o-grid.o-grid--middle > div:nth-child(1) > div > div > div.c-select__textbox');
+$btnFormat->click();  //ouvre le select
 $excelFormat = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div > div > form > div.o-grid.o-grid--middle > div:nth-child(1) > div > div > div.c-select__listbox.is-open > div:nth-child(9)');
 $excelFormat->click();
 
 $btnDownload = $page->find('css','input[value="Télécharger"]');
 $btnDownload->click();
 
-$date = $page->find('css', '#quote_search_endDate')->getValue();
-$date = str_replace('/', '-', $date);
-$date = strtotime($date);
-$date = date('Y-m-d', $date);
-$date = str_replace('-', '_', $date);
-var_dump($date);
+sleep(5);
+// $date = $page->find('css', '#quote_search_endDate')->getValue();
+// $date = str_replace('/', '-', $date);
+// $date = strtotime($date);
+// $date = date('Y-m-d', $date);
+// $date = str_replace('-', '_', $date);
+// var_dump($date);
 
-$f="SRD_".$date.".xls";
-$essai = 1;
-while (!file_exists($f) && $essai<10) {
-  sleep(1);
-  $essai++;
-}
+// $f="SRD_".$date.".xls";
+// $essai = 1;
+// while (!file_exists($f) && $essai<10) {
+//   sleep(1);
+//   $essai++;
+// }
 
-
-system('pause');
+// system('pause');
--- front/history.php
+++ front/history.php
@@ -12,14 +12,31 @@ $webpage = new WebPage("History");
 $webpage->appendContent(<<<HTML
     <div class="">
         <form action="../api/scrapingHistorique.php" method="post">
-            <label for="url-type-styled-input">URL de l'historique à WebScraper </label>
-            <input type="url" name="url" id="url-type-styled-input" placeholder="https://www.boursorama.com/cours/...">
-        
-            <button type="submit">scraper</button>
+            <label for="actions-select">Historique à télécharger </label>
+            <select name="actions" id="actions-select">
+                <option value="1">Actions éligibles au SRD</option>
+                <option value="2">Compartiment A</option>
+                <option value="3">Compartiment B</option>
+                <option value="4">Compartiment C</option>
+                <option value="5">Euronext Growth</option>
+                <option value="6">Autres titres</option>
+                <option value="7">Euronext Access</option>
+                <option value="8">Indice CAC 40</option>
+                <option value="9">Indice SBF 120</option>
+                <option value="10">Indice CAC All-Tradable</option>
+                <option value="11">Toutes les actions bourses de Paris</option>
+                <option value="12">Fond</option>
+                <option value="13">Trackers</option>
+                <option value="14">Warrants</option>
+                <option value="15">Indices Paris</option>
+                <option value="16">ESTX50 EUR P</option>
+                <option value="17">Obligations</option>
+            </select>
+            <button type="submit">Télécharger</button>
         </form> 
     </div>
 HTML);
-  
+
 
 
 
