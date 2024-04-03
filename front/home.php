cf0e9124a3b58f6ed5559656713f8705fc6af8f4
Marine Legendre <marine.leg02@gmail.com>
2024-04-03 15:43:23 +0200

    wip: style

--- front/home.php
+++ front/home.php
@@ -11,15 +11,18 @@ $webpage = new WebPage("Home");
 
 $webpage->appendContent(<<<HTML
     <div class="p-3">
+        <h2 class="mb-3">Scraper une action</h2>
         <form action="../api/scrapingAction.php" method="post">
-            <label for="url-type-styled-input">URL de cours à WebScraper</label>
-            <div class="input-group flex-nowrap mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="basic-addon3">https://www.boursorama.com/cours/</span>
-                </div>
-                <input type="text" name="code" class="form-control" id="url-type-styled-input" aria-describedby="basic-addon3">
-                <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Scraper</button>
+            <div class="form-group">
+                <label for="url-type-styled-input">Code du cours à WebScraper</label>
+                <div class="input-group flex-nowrap mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon3">https://www.boursorama.com/cours/</span>
+                    </div>
+                    <input type="text" name="code" class="form-control" id="url-type-styled-input" aria-describedby="basic-addon3">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Scraper</button>
+                    </div>
                 </div>
             </div>
         </form> 
