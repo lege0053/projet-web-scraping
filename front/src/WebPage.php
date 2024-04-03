9f4ebc6585f3111fe3acce31f8f44975be2d2047
Marine Legendre <marine.leg02@gmail.com>
2024-04-03 19:19:51 +0200

    feat: ico ✨

--- front/src/WebPage.php
+++ front/src/WebPage.php
@@ -191,7 +191,7 @@ class WebPage
             <head>
                     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
                     <title>{$this->getTitle()}</title>
-                    <link rel="icon" type="image/png" href="img/favicon.png">
+                    <link rel="icon" type="image/png" href="src/img/bourse.png">
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
                     <!-- <link rel="stylesheet" href="./src/css/style.css" > -->
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
@@ -201,7 +201,7 @@ class WebPage
                
                   
             </head>
-            <html  lang="fr">
+            <html lang="fr">
                 <body>
                     <header>
                         <nav class="navbar navbar-expand-sm navbar-light bg-light">
