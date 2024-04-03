9dcf305b2bdc6883f492070956dbeb66a5a2a7c7
Marine Legendre <marine.leg02@gmail.com>
2024-04-03 14:31:57 +0200

    feat: choix possible entre la bourse de paris ou bourse international ✨

--- api/scrapingHistorique.php
+++ api/scrapingHistorique.php
@@ -12,8 +12,6 @@ $mink = new Mink(array(
   'browser' => new Session(new ChromeDriver('http://localhost:9222', null, 'http://www.google.com', ['downloadBehavior'=> 'allow','downloadPath'=> __DIR__])),
 ));
 
-$actions = $_POST['actions'];
-
 $mink->setDefaultSessionName('browser');
 $session = $mink->getSession();
 
@@ -41,11 +39,18 @@ if($btnMembre) {
   sleep(2);
 }
 
-
-$session->visit('https://www.boursorama.com/connexion/?org=/espace-membres/telecharger-cours/paris');
+if(isset($_POST['actions'])) {
+  // actions bourse de paris
+  $actions = $_POST['actions'];
+  $session->visit('https://www.boursorama.com/connexion/?org=/espace-membres/telecharger-cours/paris');
+} else if(isset($_POST['actions-int'])) {
+  // actions international
+  $actions = $_POST['actions-int'];
+  $session->visit('https://www.boursorama.com/connexion/?org=/espace-membres/telecharger-cours/international');
+}
 $page = $session->getPage();
 
-// actions
+// select action
 $btnActions = $page->find('css', '#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div > div > form > div:nth-child(3) > div > ul > li.o-list__item.u-1\/2\@sm-min.u-1\/3\@md-min > div.c-select.o-accordion-content > div > div.c-select__textbox');
 $btnActions->click(); //ouvre le select
 $selectActions = $page->find('css','#main-content > div > div.l-basic-page > div.l-basic-page__sticky-container > div.l-basic-page__main > div > div > form > div:nth-child(3) > div > ul > li.o-list__item.u-1\/2\@sm-min.u-1\/3\@md-min > div.c-select.o-accordion-content.is-expanded > div > div.c-select__listbox.is-open > div:nth-child('.$actions.')');
@@ -61,18 +66,3 @@ $btnDownload = $page->find('css','input[value="Télécharger"]');
 $btnDownload->click();
 
 sleep(5);
-// $date = $page->find('css', '#quote_search_endDate')->getValue();
-// $date = str_replace('/', '-', $date);
-// $date = strtotime($date);
-// $date = date('Y-m-d', $date);
-// $date = str_replace('-', '_', $date);
-// var_dump($date);
-
-// $f="SRD_".$date.".xls";
-// $essai = 1;
-// while (!file_exists($f) && $essai<10) {
-//   sleep(1);
-//   $essai++;
-// }
-
-// system('pause');
--- front/history.php
+++ front/history.php
@@ -11,8 +11,9 @@ $webpage = new WebPage("History");
 
 $webpage->appendContent(<<<HTML
     <div class="">
+        <h1 class="titre">Historique à télécharger</h1>
         <form action="../api/scrapingHistorique.php" method="post">
-            <label for="actions-select">Historique à télécharger </label>
+            <label for="actions-select">Bourse de Paris : </label>
             <select name="actions" id="actions-select">
                 <option value="1">Actions éligibles au SRD</option>
                 <option value="2">Compartiment A</option>
@@ -31,6 +32,23 @@ $webpage->appendContent(<<<HTML
                 <option value="15">Indices Paris</option>
                 <option value="16">ESTX50 EUR P</option>
                 <option value="17">Obligations</option>
+            </select>
+            <button type="submit">Télécharger</button>
+        </form> 
+
+        <form action="../api/scrapingHistorique.php" method="post">
+            <label for="actions-international">International : </label>
+            <select name="actions-int" id="aactions-international">
+                <option value="1">Etats Unis : Indice Nasdaq 100</option>
+                <option value="2">Allemagne : Indice DAX</option>
+                <option value="3">Royaume-Uni : Indice Footsie 100</option>
+                <option value="4">Belgique : Indice BEL20</option>
+                <option value="5">Suisse : Indice SMI</option>
+                <option value="6">Espagne : Indice IBEX35</option>
+                <option value="7">Pays-Bas : Indice AEX25</option>
+                <option value="8">Italie: Indice FTSE MIB</option>
+                <option value="9">Indices internationaux</option>
+                <option value="10">Devises</option>
             </select>
             <button type="submit">Télécharger</button>
         </form> 
