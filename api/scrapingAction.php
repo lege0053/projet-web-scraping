81699d878e62bc6eb5d2d35c594c3b9e00a170bd
lucie <fosssierlucie@gmail.com>
2024-04-09 11:29:59 +0200

    feat: j'ai tout mis en json ouah

--- api/scrapingAction.php
+++ api/scrapingAction.php
@@ -17,14 +17,14 @@ $mink->setDefaultSessionName('browser');
 $session = $mink->getSession();
 
 $code = $_POST['code'];
-var_dump($code);
+//var_dump($code);
 
 $session->visit("https://www.boursorama.com/cours/".$code);
 $page = $session->getPage();
-
+$date = (new DateTime())->format('Y-m-d H:i:s');
 // récupération des données
 $data = array(
-  'dateHours' => (new DateTime())->format('Y-m-d H:i:s'),
+  'dateHours' => $date,
   'label' => $page->find('css','a.c-faceplate__company-link')->getText(),
   'last' => $page->find('css','span.c-instrument.c-instrument--last')->getText(),
   'aOpen' => $page->find('css','span.c-instrument--open')->getText(),
@@ -34,14 +34,17 @@ $data = array(
   'low' => $page->find('css','span.c-instrument--low')->getText(),
   'totalVolume' => $page->find('css','span.c-instrument--totalvolume')->getText()
 );
+$json_data = json_encode($data);
 
-var_dump($data);
+echo $json_data;
 
-$req = MyPDO::getInstance()->prepare(<<<SQL
-        INSERT INTO action (`dateHours`, `label`, `last`, `aClose`, `aOpen`, `currency`, `high`, `low`, `totalVolume`) 
-        VALUES (:dateHours, :label, :last, :aOpen, :aClose, :currency, :high, :low, :totalVolume)
-SQL);
+//var_dump($data);
 
-$req->execute($data);
+//$req = MyPDO::getInstance()->prepare(<<<SQL
+  //      INSERT INTO action (`dateHours`, `label`, `last`, `aClose`, `aOpen`, `currency`, `high`, `low`, `totalVolume`) 
+    //    VALUES (:dateHours, :label, :last, :aOpen, :aClose, :currency, :high, :low, :totalVolume)
+//SQL);
+
+//$req->execute($data);
 
 
